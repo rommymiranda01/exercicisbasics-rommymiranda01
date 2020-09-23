9646ace686d5a5650a2a5a043e3176e0b42fe915
rommy.miranda <rommy.miranda@cirvianum.cat>
2020-09-23 13:11:53 +0200

    Titol
    Exercici 1,2,3

---
+++ src/Horari.java
@@ -0,0 +1,5 @@
+public class Horari {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        float x= 11;
+        float y= 3;
+        //System.out.println(x+y);
+        //System.out.println(x-y);
+        //System.out.println(x*y);
+        //System.out.println(x/y);
+        System.out.println(x%y);
+    }
+}
